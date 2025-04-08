aab9a9c6082e688fcff00405f8764660f2b906b4
Wellington B. Corrêa <wellington.bcrr@gmail.com>
2025-04-07 23:01:47 -0300

    +JS (Repetições)

---
+++ aula13/ambiente.js
@@ -0,0 +1,17 @@
+var c = 2
+while (c <= 5) {
+    console.log(`X ${c}`)
+    c++
+}
+
+do {
+    console.log(`Y ${c}`)
+    c++
+} while (c <= 7)
+
+
+
+
+
+//console.log ('Tudo bem?')
+
---
+++ aula14/ambiente2.js
@@ -0,0 +1,10 @@
+for (c=1;c<=14;c++) {
+    console.log(`Etapa: ${c}`)
+
+}
+
+/*
+
+for (Inicialização ; Teste Lógico ; Incremento) {
+    
+}
