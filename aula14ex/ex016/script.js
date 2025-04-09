34cf47a949cf3b4cf941745bc9c1e523742356c6
Wellington B. Corrêa <wellington.bcrr@gmail.com>
2025-04-08 23:11:21 -0300

    Começando 2 mini projetos

---
+++ aula14ex/ex016/script.js
@@ -0,0 +1,10 @@
+var inicio = window.document.getElementById('inicio-contador')
+var fim = window.document.getElementById('fim-contador')
+var passo = window.document.getElementById('passo-contador')
+
+
+function contar() {
+    if (inicio = undefined) {
+        window.alert('[ERRO] Inicio da contagem não indicado')
+    }
+}
